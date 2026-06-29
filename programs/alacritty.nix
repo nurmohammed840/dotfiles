1737e97e1443fe604e7708645bb3197a402f561d
Nur Mohammed <nurmohammed840@gmail.com>
2026-06-29 18:36:45 +0600

    Remove copy key binding from Alacritty configuration

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -4,16 +4,11 @@
 
     settings = {
       keyboard.bindings = [
-        {
-          mods = "Control";
-          key = "C";
-          action = "Copy";
-        }
-        {
-          mods = "Control";
-          key = "V";
-          action = "Paste";
-        }
+        # { mods = "Control"; key = "C"; action = "Copy"; }
+        { mods = "Control"; key = "V"; action = "Paste"; }
+        
+        # See: https://github.com/alacritty/alacritty/issues/474#issuecomment-338803299
+        { mods = "Control"; key = "Back"; chars = "\u001b\u007f"; } # Delete word
       ];
     };
   };
