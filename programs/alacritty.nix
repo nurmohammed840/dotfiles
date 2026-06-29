2d2ca5d9c141d48c719102d74aa53e1b3171a16d
Nur Mohammed <nurmohammed840@gmail.com>
2026-06-29 18:40:29 +0600

    Fix escape sequence in Alacritty key binding for word deletion

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -6,9 +6,9 @@
       keyboard.bindings = [
         # { mods = "Control"; key = "C"; action = "Copy"; }
         { mods = "Control"; key = "V"; action = "Paste"; }
-        
+
         # See: https://github.com/alacritty/alacritty/issues/474#issuecomment-338803299
-        { mods = "Control"; key = "Back"; chars = "\u001b\u007f"; } # Delete word
+        { mods = "Control"; key = "Back"; chars = "\\u001b\\u007f"; } # Delete word
       ];
     };
   };
