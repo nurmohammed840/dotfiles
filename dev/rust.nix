24afc043cacf80e8beef6670083473a97bb81bc0
Nur Mohammed <nurmohammed840@gmail.com>
2026-07-11 06:51:40 +0600

    Updated system

--- dev/rust.nix
+++ dev/rust.nix
@@ -13,11 +13,25 @@
     # [profile.dev.package."*"]
     # opt-level = 3
 
+    [profile.dev.build-override]
+    opt-level = 3
+
+    [profile.dev]
+    debug = 0
+    opt-level = 0
+    strip = "debuginfo"
+
     [build]
+    incremental = true
     rustc-wrapper = "sccache"
 
     [target.x86_64-unknown-linux-gnu]
     linker = "clang"
-    rustflags = ["-C", "link-arg=-fuse-ld=mold"]
+    rustflags = [
+      # (Nightly) Make the current crate share its generic instantiations
+      "-Zshare-generics=y",
+      "-Z", "threads=16",
+      "-C", "link-arg=-fuse-ld=mold",
+    ]
   '';
 }
