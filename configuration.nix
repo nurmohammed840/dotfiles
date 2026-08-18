a154e2bd0414d7d7cfb5b24f67e55b9a53fcfa98
Nur Mohammed <nurmohammed840@gmail.com>
2026-08-18 18:19:25 +0600

    Comment out gamemode and gamescope program settings in configuration

--- configuration.nix
+++ configuration.nix
@@ -64,8 +64,8 @@
   environment.systemPackages = with pkgs; [];
 
   programs.steam.enable = true;
-  programs.gamemode.enable = true;
-  programs.gamescope.enable = true;
+  # programs.gamemode.enable = true;
+  # programs.gamescope.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
